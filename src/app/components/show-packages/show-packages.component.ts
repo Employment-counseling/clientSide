f00943928767c590c163f66c26bea89da7ef55f9
tphoto452@gmail.com <ToviSmotni>
2025-03-04 11:00:25 +0200

    HomePage

--- src/app/components/show-packages/show-packages.component.ts
+++ src/app/components/show-packages/show-packages.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';  // ייבוא מודול כפתור
+
 
 @Component({
   selector: 'app-show-packages',
   standalone: true,
   templateUrl: './show-packages.component.html', 
+   imports: [ButtonModule],
   styleUrls: ['./show-packages.component.css']  
 })
 export class ShowPackagesComponent {
 
-  selectOption(option: string) {
-    alert(`בחרת את האפשרות: ${option}`);
+  onConsultationClick() {
+    console.log("ייעוץ תעסוקתי נבחר");
+  }
+
+  onGraphologyClick() {
+    console.log("גרפולוגיה נבחרה");
+  }
+
+  onMeetingClick() {
+    console.log("פגישת ייעוץ נבחרה");
   }
 
 }
