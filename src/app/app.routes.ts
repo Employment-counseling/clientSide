bf9cc65df5b2eaee2276e10c60e1d2fbf739bac8
tphoto452@gmail.com <ToviSmotni>
2025-06-26 16:33:39 +0300

    Package selection page styling almost complete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
+import { QuizSelectorComponent } from './components/quiz-selector/quiz-selector.component';
+
 
 export const routes: Routes = [
    // { path:"" ,component:LoginPageComponent}
-     { path:"" ,component:HomePageComponent},
+     { path:"" ,component:QuizSelectorComponent},
      { path:"Login" ,component:LoginPageComponent}
 ];
---
+++ src/app/components/quiz-selector/quiz-selector.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-selector',
+  standalone: true,
+  imports: [],
+  templateUrl: './quiz-selector.component.html',
+  styleUrl: './quiz-selector.component.css'
+})
+export class QuizSelectorComponent {
+
+}
